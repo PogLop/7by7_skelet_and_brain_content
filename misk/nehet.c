9710d35a367f965767531dd4388e0a6131bbdc61
Prokop Schovanec <poklop@Prokop--MacBook.local>
2026-02-16 23:04:05 +0100

    dobrou

--- misk/nehet.c
+++ misk/nehet.c
@@ -1,13 +1,20 @@
-#include "../komplex_budov_data.h"
 #include "nehet.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdint.h>
 
 //data se prenasi tak jak jsou, je treba znat strukturu aby se to dalo dekodovat. asi ok?
 //pointless copying of ints into chars and wasting memory, what a day
 
 //...1 023
+
+//to be precise, this profoundly random collection of functions turns internal
+//representation of hardware matrix to format, that can be easily transoprtable 
+//óver údp network protocol to pure data, where it will be parsed as FUDI implemeted
+//by fudis.c fudis.h and then finally consumed by objects as "simple_matrix~" or 
+//"pnukget" to simplify sounding patch creationalism.
 
 char *FORMATRIX(int *matrix, int sizea, int sizeb) 
 {
@@ -19,7 +26,7 @@ char *FORMATRIX(int *matrix, int sizea, int sizeb)
     {
         for(b = 0; b < sizeb; b++)
         {
-            inde = (a * MATRIX_SIZE) + b;
+            inde = (a * sizeb) + b;
             turin[inde] = *(matrix + inde) - '0';
         }
     }
@@ -41,7 +48,7 @@ int **UNFORMATRIX(char *matrix, int sizea, int sizeb)
     {
         for(y = 0; y < sizeb; y++)
         {
-            index = (h * MATRIX_SIZE) + y;
+            index = (h * sizeb) + y;
             r[h][y] = matrix[index] + '0';
         }
     }
@@ -49,14 +56,44 @@ int **UNFORMATRIX(char *matrix, int sizea, int sizeb)
     return r;
 }
 
-char *FORMATPNUKTRIX(int *matrix, int sizea, int sizeb, int sizec)
+char *FORMATPNUKTRIX(int16_t *matrix, int sizea, int sizeb, int sizec, int offset)
 {
-    char *r = (char *)malloc(sizea * sizeb * (sizec * 3)); //sizec -->– kolik je pnuku; 3 -->- 3 digit value
+    char *r = malloc(sizea * sizeb * sizec * sizeof(int16_t) + offset); //predikce (7 * 7 * 3 * 2) + 6
+    int16_t a, b, c, i, num;
+    char sp = '\x20'; 
+    unsigned char byts[2];
+
+    //snprintf(r, 5, "list%c", sp); manualne
+
+    for(a = 0; a < sizea; a++)
+    {  
+        for(b = 0; b < sizeb; b++)
+        {
+            for(c = 0; c < (sizec * sizeof(int16_t)); c++)
+            {
+                i = (a + sizea * (b + sizec * c)) + offset; //5 -->- list offset napriklad
+                num = *(matrix + i);
+                
+                byts[0] = num >> 8;
+                byts[1] = num & 0x00FF; //lower part
+
+                r[i] = byts[0];
+                r[i + 1] = byts[1];
+                
+                c++;
+            }
+        }
+    }
 
     return r;
 }
 
-int *UNFORMATPNUKTRIX(char *matrix, int sizea, int sizeb, int sizec);
+
+//////////
+int *UNFORMATPNUKTRIX(char *matrix, int sizea, int sizeb, int sizec); 
+//neni treba, diky tomu, ze FUDI pd dostane list 
+//a s tim uz se da pracovat 
+/////////////
 
 int DIDTHEMATRIXCHANGE(int *matrix, int c, ...) //dimensions
 {
--- misk/nehet.h
+++ misk/nehet.h
@@ -1,10 +1,15 @@
 #pragma once
-#include "../komplex_budov_data.h"
 
+#include <stdint.h>
 char *FORMATRIX(int *matrix, int sizea, int sizeb);
 int **UNFORMATRIX(char *matrix, int sizea, int sizeb);
 
-char *FORMATPNUKTRIX(int *matrix, int sizea, int sizeb, int sizec);
+//ver specific function, that turns 3 dimensional array into single dimension
+//array formatted as partial standart fudi list (the list keyword is missing),
+//thats the cause of "offset" variable and ";" is absent, so you will need
+//to add it manualy
+char *FORMATPNUKTRIX(int16_t *matrix, int sizea, int sizeb, int sizec, int offset);
+
 int *UNFORMATPNUKTRIX(char *matrix, int sizea, int sizeb, int sizec);
 
 int DIDTHEMATRIXCHANGE(int *matrix, int count, ...);
